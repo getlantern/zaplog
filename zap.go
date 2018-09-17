c77474c5b9e787b5341e4285acd9b0ea4a8187b8
Myles Horton <myles@getlantern.org>
2018-09-17 13:29:42 -0700

    zap wrappers

---
+++ zap.go
@@ -0,0 +1,29 @@
+package zaplog
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+)
+
+var (
+	zapConfig = zap.NewProductionConfig()
+	zlog      *zap.SugaredLogger
+	once      sync.Once
+)
+
+// SetZapConfig allows users to customize the configuration. Note this must be called before
+// any logging takes place -- it will not reset the configuration of an existing logger.
+func SetZapConfig(config zap.Config) {
+	zapConfig = config
+}
+
+// ZapLogger creates a new zap logger with the specified name.
+func ZapLogger(prefix string) *zap.SugaredLogger {
+	once.Do(func() {
+		baseLog, _ := zapConfig.Build()
+		// Make sure our wrapper code isn't what always shows up as the caller.
+		zlog = baseLog.Sugar()
+	})
+	return zlog.Named(prefix)
+}
---
+++ zap_test.go
@@ -0,0 +1,21 @@
+package zaplog
+
+import (
+	"testing"
+
+	"go.uber.org/zap"
+)
+
+func TestBasicLogging(t *testing.T) {
+	SetZapConfig(zap.NewDevelopmentConfig())
+
+	log := ZapLogger("tester")
+	log.Error("test")
+
+	log.Errorf("Error %v", "bop")
+
+	log.Debug("test")
+	log.Info("test")
+	log.Debugf("test %v", "test")
+	log.Infof("test %v", "test")
+}
